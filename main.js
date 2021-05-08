c08b814a6dca5a6acba0a119e51bc084b2c33977
dj4t9n <pandeyjatin47@gmail.com>
2021-05-08 17:49:30 +0530

    problem in rendering

--- main.js
+++ main.js
@@ -22,39 +22,86 @@ class Generation {
   }
 
   setNextState(){
-    for (var i = 0; i < this.size; i++){
-      for (var j = 0; j < this.size; j++){
+    for (var i = 1; i < this.size - 1; i++){
+      for (var j = 1; j < this.size - 1; j++){
         this.member[i][j].next = this.getNextState(i, j)
       }
     }
   }
-  getNextState(i, j){
-    if (i === 0 || j === 0 || i === this.size - 1 || j === this.size - 1){
-      return 0
-    }
-    else{
-      let left      = this.member[i-1][j].current 
-      let right     = this.member[i+1][j].current 
-      let up        = this.member[i][j-1].current 
-      let down      = this.member[i][j+1].current 
-      let leftUp    = this.member[i-1][j-1].current 
-      let leftDown  = this.member[i-1][j+1].current 
-      let rightUp   = this.member[i+1][j-1].current 
-      let rightDown = this.member[i+1][j+1].current 
+  
+  setState(i, j, val){
+    this.member[i][j].current = val
+  }
 
-      if (left == right){
+  getNextState(i, j){
+    var neighbourhood = new Array(
+      this.member[i-1][j].current, 
+      this.member[i+1][j].current, 
+      this.member[i][j-1].current, 
+      this.member[i][j+1].current, 
+      this.member[i-1][j-1].current, 
+      this.member[i-1][j+1].current, 
+      this.member[i+1][j-1].current, 
+      this.member[i+1][j+1].current 
+    )
+
+    let count = 0
+    let alive
+    if (this.member[i][j] === 1){
+      alive = true
+    }else {
+      alive = false
+    }
+    for (var k = 0; k < 8; k++){
+      if (neighbourhood[k] === 1) {
+        count ++
+      }
+    }
+    if (alive){
+      if (count < 2){
+        return 0
+      }
+      else if (count < 4){
         return 1
       }
       else {
         return 0
       }
+    }else {
+      if (count === 3){
+        return 1
+      }else {
+        return 0
+      }
     }
+  }
+  render(){
+    var context = ""
+    for (var i = 0; i < this.size; i++){
+      for (var j = 0; j < this.size; j++){
+        if (i === 0 || i === this.size - 1 || j === 0 || j === this.size - 1 ){
+          context += '#'
+        }
+        else if (this.member[i][j] === 1){
+          context += '@'
+        }else {
+          context += ' '
+        }
+      }
+      context += '\n'
+    }
+    console.log(context)
   }
 }
 
-gen = new Generation(10)
-console.log(gen)
-
-console.log(gen.member[1][1].current)
-gen.setNextState()
-console.log(gen.member[1][1].next)
+gen = new Generation(50)
+gen.setState(3, 4, 1)
+gen.setState(4, 4, 1)
+gen.setState(3, 3, 1)
+gen.setState(3, 5, 1)
+setInterval(main, 10000)
+function main(){
+  console.clear()
+  gen.render()
+  gen.setNextState()
+}
---
+++ test.js
@@ -0,0 +1,8 @@
+function main(){
+  console.clear()
+  for(var i = 0; i < 100; i++){
+    console.log(i + '\n')
+  }
+}
+
+setInterval(main, 2000)
