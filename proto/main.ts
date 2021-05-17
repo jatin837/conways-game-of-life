3266ec5adad7356a5250f904c9ce187272bb0b7f
dj4t9n <pandeyjatin47@gmail.com>
2021-05-17 23:30:30 +0530

    just changed var to let :>

--- proto/main.ts
+++ proto/main.ts
@@ -1,11 +1,11 @@
-var canvas = document.getElementById("screen") as HTMLCanvasElement;
-var ctx : CanvasRenderingContext2D = canvas.getContext('2d');
+jjkkjlet canvas = document.getElementById("screen") as HTMLCanvasElement;
+let ctx : CanvasRenderingContext2D = canvas.getContext('2d');
 
-var blockSize = 9;
-var toPx = function (x: number) { return x * blockSize; };
-var x_blocks : number = 110;
-var y_blocks : number = 70;
-var border : number = 1;
+let blockSize = 9;
+let toPx = function (x: number) { return x * blockSize; };
+let x_blocks : number = 110;
+let y_blocks : number = 70;
+let border : number = 1;
 canvas.height = toPx(y_blocks);
 canvas.width = toPx(x_blocks);
 
@@ -14,33 +14,33 @@ ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 function drawBoard(m: number, n: number) {
     ctx.strokeStyle = 'white';
-    for (var i = 1; i < m; i++) {
+    for (let i = 1; i < m; i++) {
         ctx.beginPath();
         ctx.lineWidth = border;
         ctx.moveTo(toPx(i), toPx(0));
         ctx.lineTo(toPx(i), toPx(n + 1));
         ctx.stroke();
     }
-    for (var i = 1; i < n; i++) {
+    for (let i = 1; i < n; i++) {
         ctx.beginPath();
         ctx.lineWidth = 1;
         ctx.moveTo(toPx(0), toPx(i));
         ctx.lineTo(toPx(m + 1), toPx(i));
         ctx.stroke();
     }
-    for (var i = 0; i < m; i++) {
+    for (let i = 0; i < m; i++) {
         ctx.fillStyle = 'white';
         ctx.fillRect(toPx(i), toPx(0), blockSize, blockSize);
     }
-    for (var i = 0; i < m; i++) {
+    for (let i = 0; i < m; i++) {
         ctx.fillStyle = 'white';
         ctx.fillRect(toPx(i), toPx(n - 1), blockSize, blockSize);
     }
-    for (var i = 0; i < n; i++) {
+    for (let i = 0; i < n; i++) {
         ctx.fillStyle = 'white';
         ctx.fillRect(toPx(0), toPx(i), blockSize, blockSize);
     }
-    for (var i = 0; i < n; i++) {
+    for (let i = 0; i < n; i++) {
         ctx.fillStyle = 'white';
         ctx.fillRect(toPx(m - 1), toPx(i), blockSize, blockSize);
     }
@@ -51,7 +51,7 @@ function drawCell(x: number, y: number, color: string) {
 }
 drawBoard(x_blocks, y_blocks);
 
-var gen = new Array(x_blocks)
+let gen = new Array(x_blocks)
 
 for (let i = 0; i < x_blocks; i++) {
     gen[i] = new Array(y_blocks)
@@ -61,15 +61,15 @@ for (let i = 0; i < x_blocks; i++) {
 }
 
 function setNextState() {
-    for (var i = 1; i < x_blocks - 1; i++) {
-        for (var j = 1; j < y_blocks - 1; j++) {
+    for (let i = 1; i < x_blocks - 1; i++) {
+        for (let j = 1; j < y_blocks - 1; j++) {
             gen[i][j][1] = getNextState(i, j);
         }
     }
 }
 
 function getNextState(i: number, j: number) {
-    var neighbourhood = new Array(gen[i - 1][j][0],
+    let neighbourhood = new Array(gen[i - 1][j][0],
         gen[i + 1][j][0],
         gen[i][j - 1][0],
         gen[i][j + 1][0],
@@ -78,8 +78,8 @@ function getNextState(i: number, j: number) {
         gen[i + 1][j - 1][0],
         gen[i + 1][j + 1][0]
     );
-    var count : number = 0
-    for (var k = 0; k < 8; k++) {
+    let count : number = 0
+    for (let k = 0; k < 8; k++) {
         if (neighbourhood[k] === 1) {
             count++;
         }
@@ -103,14 +103,14 @@ function getNextState(i: number, j: number) {
 }
 
 function setInitialState(initStates: number[][]) {
-    for (var i = 0; i < initStates.length; i++) {
+    for (let i = 0; i < initStates.length; i++) {
         setState(initStates[i][0], initStates[i][1], 1);
     }
 }
 
 function evolve() {
-    for (var i = 0; i < x_blocks; i++) {
-        for (var j = 0; j < y_blocks; j++) {
+    for (let i = 0; i < x_blocks; i++) {
+        for (let j = 0; j < y_blocks; j++) {
             gen[i][j][0] = gen[i][j][1];
         }
     }
